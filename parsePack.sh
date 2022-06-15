3795315037597365d42e1822f4006d336cee66d6
Micael Araujo <micael12jose@gmail.com>
2022-06-15 23:22:09 +0100

    Removed forge installer execution

--- parsePack.sh
+++ parsePack.sh
@@ -35,20 +35,4 @@ if [[ ! -d mods ]]; then
 	cd ..
 fi
 
-# fetch forge
-modloader="$(cat manifest.json | jq -r '.minecraft | "\(.version),\(.modLoaders[] | select(.primary == true).id)"' | head -n1)"
-if [[ $modloader == *"forge"* ]]; then
-	mc=${modloader/,*/}
-	forge=${modloader/*,forge-/}
-
-	# handling for old forge binary names
-	if [[ $(grep -Poh "(1\.10$|1\.9|1\.8|1\.7|1\.6|1\.5|1\.4|1\.3|1\.2|1\.1$)" <<< "$mc") == '' ]]; then	
-		curl -L -O "https://maven.minecraftforge.net/net/minecraftforge/forge/$mc-$forge/forge-$mc-$forge-installer.jar"
-		java -jar "forge-$mc-$forge-installer.jar"
-	else
-		curl -L -O "https://maven.minecraftforge.net/net/minecraftforge/forge/$mc-$forge-$mc/forge-$mc-$forge-$mc-installer.jar"
-		java -jar "forge-$mc-$forge-$mc-installer.jar"
-	fi
-else
-	echo "mmmm my attention span is too short for fabric rn"
-fi
+ 
